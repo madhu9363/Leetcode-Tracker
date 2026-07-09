5040b967a9e394c149cf9db42fdc8518cb5a623f
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:08:23 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.java
@@ -0,0 +1,10 @@
+// Last updated: 09/07/2026, 15:08:22
+import java.util.Scanner;
+
+class Solution {
+
+        public int sum (int num1, int num2){
+            return num1+num2;
+        }
+        }
+    
