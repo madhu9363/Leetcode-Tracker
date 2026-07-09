5c17dd03c6296613892bcffdb66a5cc27c5e8a61
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:12:44 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 15:12:44
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+}
