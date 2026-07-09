c4b637b3246bfbfad7995c8241211448d10fc841
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:07:01 +0530

    Create 4186-CountResiduePrefixes

---
+++ 4186-CountResiduePrefixes/4186-CountResiduePrefixes.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 15:07:01
+class Solution {
+    public int residuePrefixes(String s) {
+        HashSet<Character>set = new HashSet<>();
+        int count =0;
+        for(int i =0;i<s.length();i++){
+            set.add(s.charAt(i));
+            if(set.size() == (i+1)%3){
+                count++;
+            }
+        }
+        return count;
+    }
+}
