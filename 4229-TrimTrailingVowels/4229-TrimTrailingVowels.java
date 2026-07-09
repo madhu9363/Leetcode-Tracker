72f8d530c18965119c0c7058bf160feb398e0ca8
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:06:56 +0530

    Create 4229-TrimTrailingVowels

---
+++ 4229-TrimTrailingVowels/4229-TrimTrailingVowels.java
@@ -0,0 +1,10 @@
+// Last updated: 09/07/2026, 15:06:56
+class Solution {
+    public String trimTrailingVowels(String s) {
+        int i = s.length() - 1;
+        while(i >=0 && (s.charAt(i) =='a' ||s.charAt(i) =='e' ||s.charAt(i)=='i'||s.charAt(i) =='o'||s.charAt(i)=='u')){
+            i--;
+        }
+        return s.substring(0,i+1);
+    }
+}
