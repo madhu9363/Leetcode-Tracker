25c7c3165a23e10d098274dcbbf167698e96e614
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:10:58 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -0,0 +1,36 @@
+// Last updated: 09/07/2026, 15:10:57
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null &&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+        while(slow!=null){
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        while(prev!= null && head != null){
+            if(prev.val!=head.val ){
+                return false;
+            } 
+            prev= prev.next;
+            head = head.next;
+        }
+        return true;
+    }
+}
