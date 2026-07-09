fde09af9649987abfb10b014e358dca7266b777d
Madhudeepitha Sri S <jaya996565@gmail.com>
2026-07-09 15:10:50 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 15:10:49
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+   
+    }
+}
